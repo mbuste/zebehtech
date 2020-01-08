138be6f9864861b3f4fb929cdcfd1d7f7059f946
Moses Nyambu <nyamosekitando@gmail.com>
2020-01-08 11:25:43 +0300

    photos service created

---
+++ src/app/photos.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  uri = 'https://photowebbackend.herokuapp.com';
+
+  constructor(private http: HttpClient) { }
+
+  addImages(myFiles) {
+    const images = myFiles;
+
+    return this.http.post(`${this.uri}/upload-images`, images);
+  }
+
+  getPhotos() {
+    return this.http.get(`${this.uri}/images`);
+  }
+  getWeddingPhotos() {
+    return this.http.get(`${this.uri}/images/weddingphotos`);
+  }
+
+  getPotraitPhotos() {
+    return this.http.get(`${this.uri}/images/potraitphotos`);
+  }
+
+  getLifestylePhotos() {
+    return this.http.get(`${this.uri}/images/lifestylephotos`);
+  }
+  getPartiesPhotos() {
+    return this.http.get(`${this.uri}/images/partiesphotos`);
+  }
+  getBirthdayPhotos() {
+    return this.http.get(`${this.uri}/images/birthdayphotos`);
+  }
+}
+
