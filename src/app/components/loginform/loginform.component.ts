595dfb12d41837199edf8b76d869301bc8ebd65e
Moses Nyambu <nyamosekitando@gmail.com>
2020-01-09 16:51:52 +0300

    validation both email and password for login component

--- src/app/components/loginform/loginform.component.ts
+++ src/app/components/loginform/loginform.component.ts
@@ -7,44 +7,61 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./loginform.component.scss']
 })
 export class LoginformComponent implements OnInit {
-  category: String = '';
+  email: String = '';
+  pass: String = '';
 
-  constructor() {
+  constructor() { }
 
-  }
-  handleSelectChange(e) {
-    this.category = e.target.value;
-  }
+  handleEmailChange(e) { this.email = e.target.value; }
+
+  handlePassChange(e) { this.pass = e.target.value; }
 
   handleSubmit(e) {
     e.preventDefault();
     this.checkInputs();
   }
 
-  checkInputs(){
-    if(this.category.trim()===''){
-      this.setErrorFor(<HTMLElement>document.getElementById('email'), 'Email cannot be blank');
+  checkInputs() {
+    if(this.validateEmail()&& this.validatePass()){
+      
     }else{
+      console.log("failed")
+    }
+    
+  }
+
+  validatePass() {
+    if (this.pass.trim().length < 8) {
+      this.setErrorFor(<HTMLElement>document.getElementById('password'), "Password too short");
+      return false;
+    }else{
+      this.setSuccessFor(<HTMLElement> document.getElementById('password'));
+      return true;
+    }    
+  }
+
+  validateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (this.email.match(mailformat)) {
       this.setSuccessFor(document.getElementById('email'));
+      return true;
+    } else {
+      this.setErrorFor(<HTMLElement>document.getElementById('email'), 'Invalid email address');
+      return false;
     }
   }
 
-  setErrorFor(input, message){
+  setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
-    small.innerText =message;
-    formControl.className="inputBox error";
+    small.innerText = message;
+    formControl.className = "inputBox error";
   }
 
-  setSuccessFor(input){
+  setSuccessFor(input) {
     const formControl = input.parentElement;
-    formControl.className='inputBox success'; 
+    formControl.className = 'inputBox success';
   }
   ngOnInit() {
   }
-
-
-
-
-
 }
